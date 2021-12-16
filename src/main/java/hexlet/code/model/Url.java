e3f46a06b1815310286ce5527d3a69c937bf5921
sergye <sergye@gmail.com>
2021-12-16 09:42:44 +0200

    Step 3 - Add ORM

---
+++ src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,36 @@
+package hexlet.code.domain;
+
+import io.ebean.Model;
+import io.ebean.annotation.WhenCreated;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public final class Url extends Model {
+
+    @Id
+    private long id;
+
+    private String name;
+
+    @WhenCreated
+    private Instant createdAt;
+
+    public Url(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+}
