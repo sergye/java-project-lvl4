bb056f833ee27c9b1fa17d1c50ad8e0f4eac96e5
sergye <sergye@gmail.com>
2021-12-24 13:06:58 +0200

    Step 6 - Add tests

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 [![Actions Status](https://github.com/sergye/java-project-lvl4/workflows/hexlet-check/badge.svg)](https://github.com/sergye/java-project-lvl4/actions)
 [![Actions Status](https://github.com/sergye/java-project-lvl4/actions/workflows/ci-gradle.yml/badge.svg)](https://github.com/sergye/java-project-lvl4/actions)
 <a href="https://codeclimate.com/github/sergye/java-project-lvl4/maintainability"><img src="https://api.codeclimate.com/v1/badges/fcc46a022fff0fd452c3/maintainability" /></a>
+<a href="https://codeclimate.com/github/sergye/java-project-lvl4/test_coverage"><img src="https://api.codeclimate.com/v1/badges/fcc46a022fff0fd452c3/test_coverage" /></a>
 
 [Heroku](https://intense-springs-51417.herokuapp.com/)
--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -1,13 +1,123 @@
 package hexlet.code;
 
+import hexlet.code.domain.Url;
+import io.ebean.DB;
+import io.ebean.Transaction;
+import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
+public final class AppTest {
 
-    @Test
-    void emptyTest() {
-        assertThat(0).isEqualTo(0);
+    private static Javalin app;
+    private static String baseUrl;
+    private static Url existingUrl;
+    private static Transaction transaction;
+
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start(0);
+        int port = app.port();
+
+        baseUrl = "http://localhost:" + port;
+
+        existingUrl = new Url("https://ru.hexlet.io");
+        existingUrl.save();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        transaction = DB.beginTransaction();
+    }
+
+    @AfterEach
+    void afterEach() {
+        transaction.rollback();
+    }
+
+    @Nested
+    class UrlControllerTest {
+
+        @Test
+        void testAddUrl() {
+            String inputUrl = "https://edition.cnn.com/search?q=europe";
+            String outputUrl = "https://edition.cnn.com";
+            HttpResponse<String> response = Unirest.post(baseUrl + "/urls").field("url", inputUrl).asEmpty();
+
+            assertThat(response.getStatus()).isEqualTo(302);
+            assertThat(response.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+            response = Unirest.get(baseUrl + "/urls").asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(outputUrl);
+            assertThat(body).contains("Страница успешно добавлена");
+
+        }
+
+        @Test
+        void testAddExistingUrl() {
+            HttpResponse<String> response = Unirest.post(baseUrl + "/urls")
+                    .field("url", existingUrl.getName()).asEmpty();
+
+            assertThat(response.getStatus()).isEqualTo(302);
+            assertThat(response.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+            response = Unirest.get(baseUrl).asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("Страница уже существует");
+        }
+
+        @Test
+        void testAddInvalidUrl() {
+            String inputUrl = "Abracadabra";
+            HttpResponse<String> response = Unirest.post(baseUrl + "/urls").field("url", inputUrl).asEmpty();
+
+            assertThat(response.getStatus()).isEqualTo(302);
+            assertThat(response.getHeaders().getFirst("Location")).isEqualTo("/");
+
+            response = Unirest.get(baseUrl + "/urls").asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).doesNotContain(inputUrl);
+            assertThat(body).contains("Некорректный URL");
+
+        }
+
+        @Test
+        void testGetUrls() {
+            HttpResponse<String> response = Unirest.get(baseUrl + "/urls").asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(existingUrl.getName());
+        }
+
+        @Test
+        void testGetUrl() {
+            HttpResponse<String> response = Unirest.get(baseUrl + "/urls/" + existingUrl.getId()).asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(existingUrl.getName());
+        }
     }
 }
